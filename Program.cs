3dc78ed12a53743000beed310431d926095b3d80
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-09-29 13:36:43 -0600

    feat: Created draft functional program, must add additional functionality - ignore caps, spaces and punctuation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "";
+            string inputBackward = "";
+            int index = 0;
+
+            Console.WriteLine("Please enter a word to determine whether it is a palindrome: ");
+            input = Console.ReadLine();
+
+            while (input.Length > 5)
+            {
+                Console.WriteLine("The word must be no more than 5 letters. Try again: ");
+                input = Console.ReadLine();
+            }
+
+            index = input.Length - 1;
+
+            while (index >= 0)
+            {
+                
+                inputBackward += input.Substring(index, 1);
+                index--;
+            }
+
+            if (input == inputBackward)
+            {
+                Console.WriteLine("The word you entered is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"The word {input} is NOT a palindrome.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
